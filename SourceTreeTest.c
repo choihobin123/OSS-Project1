1400b62a6d3345a4b354a4aedc0c6802fd12251f
choihobin <yyzx1025@gmail.com>
2025-10-29 17:10:40 +0900

    Test 브랜치에 코드를 추가함

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main(){
+    int i = 0;
     printf("First OSS Code");
     printf("Secind OSS Code");
+
+    for(int i = 0; i<10;i++){
+        printf("Test 브랜치\n");
+    }
 
   return 0;
 }
